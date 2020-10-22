13d73cc1f70af6a2e5433f1601d83298c44eb09c
sahana190 <saisahana190@gmail.com>
2020-10-22 21:56:22 +0530

    Updates

---
+++ precache-manifest.3acae18b8b4b1b5eb436fb2d3ce01bf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d83005053ad26f589c9a171c5664e73",
+    "url": "/calc-logger-ui/index.html"
+  },
+  {
+    "revision": "c5897cdb4beb150fb38f",
+    "url": "/calc-logger-ui/static/css/main.4ca22134.chunk.css"
+  },
+  {
+    "revision": "c8a5988bfb5e29dd6bc7",
+    "url": "/calc-logger-ui/static/js/2.d150001f.chunk.js"
+  },
+  {
+    "revision": "2a4ea1f7636434f4b73ba62ecb947d07",
+    "url": "/calc-logger-ui/static/js/2.d150001f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5897cdb4beb150fb38f",
+    "url": "/calc-logger-ui/static/js/main.7a998319.chunk.js"
+  },
+  {
+    "revision": "449f3e6ce1f49b61f1d2",
+    "url": "/calc-logger-ui/static/js/runtime-main.84da926f.js"
+  }
+]);
